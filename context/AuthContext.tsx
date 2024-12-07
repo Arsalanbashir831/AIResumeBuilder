e184347db29d40302cd4302f17a1f613e9123c76
Arsalan831 <arsalanbashir831@gmail.com>
2024-12-07 14:10:12 +0500

    formating

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -7,129 +7,123 @@ import { login as apiLogin, register as apiRegister, getUserData, refreshAccessT
 import Spinner from "@/components/ui/Spinner";
 
 interface AuthContextType {
-    user: User | null; 
-    login: (email: string, password: string) => Promise<void>; 
-    logout: () => void; 
-    register: (email: string, password: string, name: string) => Promise<void>;
-    isAuthenticated: boolean; 
+  user: User | null;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+  register: (email: string, password: string, name: string) => Promise<void>;
+  isAuthenticated: boolean;
 }
 
 interface User {
-    id: string;
-    email: string;
-    name: string;
+  id: string;
+  email: string;
+  name: string;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 interface AuthProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-    const [user, setUser] = useState<User | null>(null);
-    const [loading, setLoading] = useState(true);
-    const router = useRouter();
-    const isAuthenticated = !!user;
-    const pathname = usePathname();
-    useEffect(() => {
-        const verifyAndFetchUser = async () => {
-          const authToken = localStorage.getItem("accessToken");
-          const refreshToken = localStorage.getItem("refreshToken");
-          if (!authToken) {    
-          
-            if (pathname !== "/signup" && pathname !== '/' ) router.push("/signin");
-            setLoading(false);
-            return;
-          }
-    
-          try {
-            // Verify the access token
-            const status = await tokenVerification(authToken);
-    
-            if (status === 200) {
-              // Token is valid, fetch user data
-              const userData = await getUserData(authToken);
-              setUser(userData);
-            } else {
-              throw new Error("Token verification failed");
-            }
-          } catch (error) {
-            console.log("Token verification error:", error);
-            try {
-              const newAuthToken = await refreshAccessToken(refreshToken);
-              localStorage.setItem("accessToken", newAuthToken.access);
-              const userData = await getUserData(newAuthToken);
-              setUser(userData);
-            } catch (refreshError) {
-              console.log("Token refresh error:", refreshError);
-    
-              // Redirect to sign-in page if refresh fails
-              if (pathname !== "/signin" ) {
-                router.push("/signin");
-              }
-              
-            }
-          } finally {
-            setLoading(false);
-          }
-        };
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const isAuthenticated = !!user;
+  const pathname = usePathname();
+  useEffect(() => {
+    const verifyAndFetchUser = async () => {
+      const authToken = localStorage.getItem("accessToken");
+      const refreshToken = localStorage.getItem("refreshToken");
+      if (!authToken) {
 
-       
-            verifyAndFetchUser();
-     
-      }, [pathname, router]);
-
-    const login = async (email: string, password: string) => {
-        try {
-            const data = await apiLogin(email, password); 
-            console.log('data', data);
-            setUser(data.user);
-            localStorage.setItem("accessToken", data.accessToken);
-            localStorage.setItem("refreshToken", data.refreshToken);
-            router.push("/dashboard");
-    
-        } catch (error: any) {
-            console.log("Login error:", error);
-            throw error.detail || "Login failed";
+        if (pathname !== "/signup" && pathname !== '/') router.push("/signin");
+        setLoading(false);
+        return;
+      }
+      try {
+        const status = await tokenVerification(authToken);
+        if (status === 200) {
+          const userData = await getUserData(authToken);
+          setUser(userData);
+        } else {
+          throw new Error("Token verification failed");
         }
-    };
-
-    const logout = () => {
-        setUser(null);
-        localStorage.removeItem("accessToken");
-        localStorage.removeItem("refreshToken");
-        router.push("/signin");
-    };
-
-    const register = async (email: string, password: string, name: string) => {
+      } catch (error) {
+        console.log("Token verification error:", error);
         try {
-            await apiRegister(email, password, name);
+          const newAuthToken = await refreshAccessToken(refreshToken);
+          localStorage.setItem("accessToken", newAuthToken.access);
+          const userData = await getUserData(newAuthToken);
+          setUser(userData);
+        } catch (refreshError) {
+          console.log("Token refresh error:", refreshError);
+          if (pathname !== "/signin") {
             router.push("/signin");
-        } catch (error: any) {
-            console.log("Registration error:", error);
-            throw error?.detail || "Registration failed";
+          }
+
         }
+      } finally {
+        setLoading(false);
+      }
     };
 
 
-    if (loading) {
-        return <div className="flex justify-center items-center h-screen">
-        <Spinner size={12} color="blue-500" />
-      </div>; 
-    }
+    verifyAndFetchUser();
 
-    return (
-        <AuthContext.Provider value={{ user, login, logout, register, isAuthenticated }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  }, [pathname, router]);
+
+  const login = async (email: string, password: string) => {
+    try {
+      const data = await apiLogin(email, password);
+      console.log('data', data);
+      setUser(data.user);
+      localStorage.setItem("accessToken", data.accessToken);
+      localStorage.setItem("refreshToken", data.refreshToken);
+      router.push("/dashboard");
+
+    } catch (error: any) {
+      console.log("Login error:", error);
+      throw error.detail || "Login failed";
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+    router.push("/signin");
+  };
+
+  const register = async (email: string, password: string, name: string) => {
+    try {
+      await apiRegister(email, password, name);
+      router.push("/signin");
+    } catch (error: any) {
+      console.log("Registration error:", error);
+      throw error?.detail || "Registration failed";
+    }
+  };
+
+
+  if (loading) {
+    return <div className="flex justify-center items-center h-screen">
+      <Spinner size={12} color="blue-500" />
+    </div>;
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout, register, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => {
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error("useAuth must be used within an AuthProvider");
-    }
-    return context;
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
 };
